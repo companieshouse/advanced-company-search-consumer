7bf311dcee852b569f6fc56a841857e27acf9933
aokunade <aokunade@companieshouse.gov.uk>
2024-02-12 10:15:36 +0000

    Added a suppression warning to avoid SonarLint warnings

--- src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplicationTests.java
@@ -1,13 +1,15 @@
 package uk.gov.companieshouse.advancedcompanysearchconsumer;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AdvancedCompanySearchConsumerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    @SuppressWarnings("squid:S2699") // at least one assertion
+    @DisplayName("Context loads")
+    @Test
+    void contextLoads() {
+    }
 
 }
