45509eb112517443c6bcbc9418005f4ee1e1f945
aokunade <aokunade@companieshouse.gov.uk>
2024-02-12 10:17:34 +0000

    replaced the tab with 4 spaces

--- src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplication.java
+++ src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdvancedCompanySearchConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvancedCompanySearchConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedCompanySearchConsumerApplication.class, args);
+    }
 
 }
