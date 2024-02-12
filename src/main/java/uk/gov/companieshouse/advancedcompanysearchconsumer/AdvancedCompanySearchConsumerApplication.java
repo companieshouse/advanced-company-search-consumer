b20268c72497630e5e1337b267c26494134b53ae
aokunade <aokunade@companieshouse.gov.uk>
2024-02-12 08:11:33 +0000

    BI-13577: Add skeleton App for Advanced-company-search-consumer

---
+++ src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.advancedcompanysearchconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedCompanySearchConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedCompanySearchConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.advancedcompanysearchconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedCompanySearchConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
