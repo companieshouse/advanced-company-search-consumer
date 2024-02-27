cbe674b7da4b08b94d9dcce794bc12dba6b18a4a
aokunade <aokunade@companieshouse.gov.uk>
2024-02-27 14:46:52 +0000

    BI-13583: Consume stream company profile messages

--- src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplicationTests.java
@@ -3,9 +3,15 @@ package uk.gov.companieshouse.advancedcompanysearchconsumer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.companieshouse.advancedcompanysearchconsumer.config.TestKafkaConfig;
 
 @SpringBootTest
+@Import(TestKafkaConfig.class)
+@ActiveProfiles("test_main_positive")
 class AdvancedCompanySearchConsumerApplicationTests {
+    
     @SuppressWarnings("squid:S2699") // at least one assertion
     @DisplayName("Context loads")
     @Test
