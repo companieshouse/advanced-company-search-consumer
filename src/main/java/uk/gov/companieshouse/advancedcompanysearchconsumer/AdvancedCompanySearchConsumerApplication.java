1234e417b46f74b20d9c13b624ee7bcf41ccac76
aokunade <aokunade@companieshouse.gov.uk>
2024-02-27 13:59:07 +0000

    BI-13583: Check for and document required environment variables for consumers

--- src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplication.java
+++ src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/AdvancedCompanySearchConsumerApplication.java
@@ -1,13 +1,19 @@
 package uk.gov.companieshouse.advancedcompanysearchconsumer;
 
-import org.springframework.boot.SpringApplication;
+import static org.springframework.boot.SpringApplication.run;
+import static uk.gov.companieshouse.advancedcompanysearchconsumer.environment.EnvironmentVariablesChecker.allRequiredEnvironmentVariablesPresent;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdvancedCompanySearchConsumerApplication {
 
+    public static final String NAMESPACE = "advanced-company-search-consumer";
+
     public static void main(String[] args) {
-        SpringApplication.run(AdvancedCompanySearchConsumerApplication.class, args);
+        if (allRequiredEnvironmentVariablesPresent()) {
+            run(AdvancedCompanySearchConsumerApplication.class, args);
+        }
     }
 
 }
