3c6fe3ffb60875ae27fbcb78d96d6c865dba46ca
Martin-Warnett <mwarnett@companieshouse.gov.uk>
2026-07-30 15:47:26 +0100

    KAF-549: Upgraded to Spring Boot 4.
    * Uplifted Spring Boot and Spring Framework versions
    * Added spring-boot-webmvc-test dependency
    * Amended deserialiser to import from Jackson 3 packages
    * Caught JacksonException in deserialiser for Jackson 3
    * Amended TestConstants to use "real" IOUtils class rather than shaded version no longer provided
    * Imported jUnit5 assertThrows
    * Added correct AutoConfigureMockMvc import to HealthCheckTest
    * Amended files to follow coding guidelines - statics first

--- src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/service/CompanyProfileDeserialiser.java
+++ src/main/java/uk/gov/companieshouse/advancedcompanysearchconsumer/service/CompanyProfileDeserialiser.java
@@ -1,13 +1,14 @@
 package uk.gov.companieshouse.advancedcompanysearchconsumer.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static uk.gov.companieshouse.advancedcompanysearchconsumer.AdvancedCompanySearchConsumerApplication.NAMESPACE;
+
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.advancedcompanysearchconsumer.exception.NonRetryableException;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-import static uk.gov.companieshouse.advancedcompanysearchconsumer.AdvancedCompanySearchConsumerApplication.NAMESPACE;
 
 @Component
 public class CompanyProfileDeserialiser {
@@ -23,7 +24,7 @@ public class CompanyProfileDeserialiser {
     public CompanyProfileApi deserialiseCompanyProfile(String data) {
         try {
             return objectMapper.readValue(data, CompanyProfileApi.class);
-        } catch (JsonProcessingException e) {
+        } catch (JacksonException e) {
             logger.errorContext( "Unable to parse message payload data", e, null);
             throw new NonRetryableException("Unable to parse message payload data", e);
         }
