13fe688a878aca68666c485c36d526610c8453bd
Martin-Warnett <mwarnett@companieshouse.gov.uk>
2026-08-04 09:41:55 +0100

    KAF-549: Addressed security vulnerabilities.
    * Uplifted log4j version numbers
    * Introduced ch-boms
    * Removed old dependencies no longer needed
    * Added tests for CompanyProfileDeserialiser
    
    Removed version / property from following dependencies brought in by ch-boms.
    
    * structured-logging
    * api-sdk-manager-java
    * ch-kafka
    * kafka-models
    * commons-lang3

---
+++ src/test/java/uk/gov/companieshouse/advancedcompanysearchconsumer/service/CompanyProfileDeserialiserTest.java
@@ -0,0 +1,52 @@
+package uk.gov.companieshouse.advancedcompanysearchconsumer.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
+import uk.gov.companieshouse.advancedcompanysearchconsumer.exception.NonRetryableException;
+import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyProfileDeserialiserTest {
+
+    @Mock
+    private ObjectMapper mockObjectMapper;
+
+    @InjectMocks
+    private CompanyProfileDeserialiser deserialiser;
+
+    @Test
+    void whenDeserialisationSuccessful_thenReturnCompanyProfileApi() throws JacksonException {
+        final var testData = "Dummy Data";
+
+        when(mockObjectMapper.readValue(testData, CompanyProfileApi.class))
+                .thenReturn(mock(CompanyProfileApi.class));
+
+        deserialiser.deserialiseCompanyProfile("Dummy Data");
+    }
+
+    @Test
+    void whenDeserialisationFails_thenThrowsException() throws JacksonException {
+        final var testData = "Dummy Data";
+        final var mockException = mock(JacksonException.class);
+
+        when(mockObjectMapper.readValue(testData, CompanyProfileApi.class))
+                .thenThrow(mockException);
+
+        final var exception = assertThrows(NonRetryableException.class, () ->
+            deserialiser.deserialiseCompanyProfile(testData));
+
+        assertThat(exception.getMessage()).isEqualTo("Unable to parse message payload data");
+        assertThat(exception.getCause()).isEqualTo(mockException);
+    }
+}
